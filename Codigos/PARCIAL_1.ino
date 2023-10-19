1279e14d2087def34f6f5515f36686491d334da4
JoaquinCarbonaro <138243106+JoaquinCarbonaro@users.noreply.github.com>
2023-10-19 01:06:46 -0300

    Add files via upload

---
+++ Codigos/PARCIAL_1.ino
@@ -0,0 +1,257 @@
+#define A 12
+#define B 13
+#define C 7
+#define D 8
+#define E 9
+#define F 11
+#define G 10
+#define SUBE 4
+#define BAJA 3
+#define RESET 5
+#define UNIDAD A4
+#define DECENA A5
+#define APAGADOS 0
+#define TIMEDISPLAYON 10
+
+int contador = 0;
+int sube = 1;
+int subePrevia = 1;
+int baja = 1;
+int bajaPrevia = 1;
+int reset = 1;
+int resetPrevia = 1;
+
+void setup()
+{
+// Configuración de pines
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+// Configuración de pines de salida para el display de 7 segmentos
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+// Inicializa el display con el valor 0
+  verEnDisplay(0);
+}
+
+void loop()
+{
+// Detecta si se presionó algún botón (SUBE, BAJA o RESET)
+  int presiona = botonPresionado();
+    
+  if (presiona == SUBE)
+  {
+  // Incrementa el contador
+    contador++;
+    if (contador > 99)
+    {
+      contador = 0;
+    }
+  }
+  
+  else if (presiona == BAJA)
+  {
+  // Decrementa el contador
+    contador--;
+    if (contador < 0)
+    {
+      contador = 99;
+    }
+  }
+  
+  else if (presiona == RESET)
+  {
+  // Reinicia el contador a 0
+    contador = 0;
+  }
+// Muestra el contador en el display
+  ContadorEntero(contador);
+}
+
+
+// Función para detectar qué botón se ha presionado
+int botonPresionado(void)
+{
+  sube = digitalRead(SUBE);
+  baja = digitalRead(BAJA);
+  reset = digitalRead(RESET);
+  
+  if (sube) //entra cuando NO presiona, osea HIGH
+    subePrevia = 1; //define estado anterior en HIGH
+  if (baja)
+    bajaPrevia = 1;
+  if (reset)
+    resetPrevia = 1;
+  
+  if (sube == 0 && sube != subePrevia) //si se presiono (LOW) y este es distinto al estado anterior
+  {
+    subePrevia = sube; //mismo valores
+    return SUBE; //devuelve define SUBE
+  }
+  if (baja == 0 && baja != bajaPrevia)
+  {
+    bajaPrevia = baja;
+    return BAJA;
+  }
+  if (reset == 0 && reset != resetPrevia)
+  {
+    resetPrevia = reset;
+    return RESET;
+  }
+  return 0;
+}
+
+
+// Función para mostrar el contador en el display de 7 segmentos
+void ContadorEntero(int numero)
+{
+    digitoEncendido(APAGADOS);
+    verEnDisplay(numero / 10);
+    digitoEncendido(DECENA);
+
+    digitoEncendido(APAGADOS);
+    verEnDisplay(numero - (numero / 10) * 10);
+    digitoEncendido(UNIDAD);
+}
+
+
+/* Función para activar un dígito (unidad o decena) - Multiplexación:
+Alternar entre los dos displays "UNIDAD" y "DECENA" para mostrar los dígitos 
+de las unidades y decenas, respectivamente, en un ciclo rápido, lo que crea 
+la ilusión de que ambos están encendidos al mismo tiempo.
+*/ 
+void digitoEncendido(int digito)
+{
+  if (digito == UNIDAD)
+  {
+    digitalWrite(UNIDAD, LOW);
+    digitalWrite(DECENA, HIGH);
+    delay(TIMEDISPLAYON);
+  }
+  else if (digito == DECENA)
+  {
+    digitalWrite(UNIDAD, HIGH);
+    digitalWrite(DECENA, LOW);
+    delay(TIMEDISPLAYON);
+  }
+  else //APAGADOS
+  {
+    digitalWrite(UNIDAD, HIGH);
+    digitalWrite(DECENA, HIGH);
+  }
+}  
+
+
+// Función para mostrar un dígito en el display de 7 segmentos
+void verEnDisplay(int digit)
+{
+// Apaga todos los segmentos
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  
+// Configura los pines para mostrar el dígito
+  switch (digit)
+  {
+    case 0:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(E, HIGH);
+      digitalWrite(F, HIGH);
+      break;
+    }
+    case 1:
+    {
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      break;
+    }
+    case 2:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(E, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 3:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 4:
+    {
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(F, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 5:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(F, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 6:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(E, HIGH);
+      digitalWrite(F, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 7:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      break;
+    }
+    case 8:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(E, HIGH);
+      digitalWrite(F, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+    case 9:
+    {
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(F, HIGH);
+      digitalWrite(G, HIGH);
+      break;
+    }
+  }
+}
---
+++ Codigos/PARCIAL_2_.ino
@@ -0,0 +1,261 @@
+#define A 12
+#define B 13
+#define C 7
+#define D 8
+#define E 9
+#define F 11
+#define G 10
+
+#define INTERRUPTOR 2
+#define UNIDAD A4
+#define DECENA A5
+
+#define APAGADOS 0
+#define TIMEDISPLAYON 10
+
+#define SENSOR_TEMPERATURA A1
+#define PIN_MOTOR A3
+
+// VARIABLES
+int numeroPrimo = 2;   // Guarda el valor del número primo
+int numero = 0;        // Guarda el valor del contador
+// Agrega dos variables para controlar el tiempo de los dos contadores
+unsigned long tiempoContadorNormal = 0;
+unsigned long tiempoContadorPrimo = 0;
+
+// Variables para la temperatura
+int temperatura;
+int umbralTemperatura = 40;  // Define el umbral de temperatura deseado en grados Celsius
+
+
+// FUNCION: SETUP
+void setup() {
+    pinMode(2, INPUT_PULLUP);
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+    pinMode(E, OUTPUT);
+    pinMode(F, OUTPUT);
+    pinMode(G, OUTPUT);
+    pinMode(UNIDAD, OUTPUT);
+    pinMode(DECENA, OUTPUT);
+    digitalWrite(UNIDAD, 0);
+    digitalWrite(DECENA, 0);
+    verEnDisplay(0);
+    pinMode(SENSOR_TEMPERATURA, INPUT);
+    pinMode(PIN_MOTOR, OUTPUT);
+}
+
+void loop() 
+{
+    int posicionSwitch = digitalRead(INTERRUPTOR);
+
+    // Leer la temperatura desde el sensor TMP36
+    int lecturaSensor = analogRead(SENSOR_TEMPERATURA);
+    float voltaje = (lecturaSensor / 1023.0) * 5.0; // Convertir lectura a voltaje
+    temperatura = (voltaje - 0.5) * 100; // Calcular la temperatura en grados Celsius
+
+    if (temperatura >= umbralTemperatura) {
+        digitalWrite(PIN_MOTOR, HIGH); // Encender el motor
+    } else {
+        digitalWrite(PIN_MOTOR, LOW); // Apagar el motor
+    }
+
+    // Mover la lógica de actualización de displays aquí cuando el motor está encendido.
+    switch (posicionSwitch) 
+    {
+    case 0:          
+        if (numero < 100) 
+        {
+            if (millis() - tiempoContadorNormal >= 1000) 
+            {  // Controla el tiempo del contador normal
+                numero++;
+                tiempoContadorNormal = millis();
+            }
+        }
+        iniciarContadorEntero(numero);
+        break;
+
+    case 1: 
+        numeroPrimo++;
+        if (numeroPrimo > 97) 
+        {
+            numeroPrimo = 0;
+        }
+        if (esPrimo(numeroPrimo)) 
+        {
+            iniciarContadorPrimo(numeroPrimo);
+            delay(1000);  // Pausa de 1 segundo
+        } 
+    }
+}
+
+// FUNCION: VER EN DISPLAY
+// La funcion tiene como objetivo el manejo de los segmentos del
+// display dependiendo del numero del contador (UNIDAD Y DECENA)                
+void verEnDisplay(int numero)
+{
+    apagarDisplays();
+    switch (numero){
+      case 0:
+      digitalWrite(A, HIGH);
+      digitalWrite(B, HIGH);
+      digitalWrite(C, HIGH);
+      digitalWrite(D, HIGH);
+      digitalWrite(E, HIGH);
+      digitalWrite(F, HIGH);
+      break;
+      case 1:
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        break;
+      case 2:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 3:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 4:
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 5:
+        digitalWrite(A, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 6:
+        digitalWrite(A, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 7:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        break;
+      case 8:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(E, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+      case 9:
+        digitalWrite(A, HIGH);
+        digitalWrite(B, HIGH);
+        digitalWrite(C, HIGH);
+        digitalWrite(D, HIGH);
+        digitalWrite(F, HIGH);
+        digitalWrite(G, HIGH);
+        break;
+    }
+}
+
+// FUNCION: INICIAR CONTADOR
+// La funcion tiene como objetivo pasarle a los display  
+// el numero que debe mostrar a traves de los segmentos 
+void iniciarContadorEntero(int numero)
+{	
+    digitoEncendido(APAGADOS);
+    verEnDisplay(numero - (numero / 10) * 10);
+    digitoEncendido(UNIDAD);
+
+  
+    digitoEncendido(APAGADOS);
+    verEnDisplay(numero / 10);
+    digitoEncendido(DECENA);
+}
+
+
+void iniciarContadorPrimo(int numeroPrimo) 
+{
+
+  digitoEncendido(APAGADOS);
+  verEnDisplay(numeroPrimo % 10);  //UNIDADES
+  digitoEncendido(UNIDAD);
+
+  
+  digitoEncendido(APAGADOS);
+  verEnDisplay(numeroPrimo / 10);  //DECENAS
+  digitoEncendido(DECENA);
+
+}
+
+// FUNCION: DIGITO ENCENDIDO
+// La funcion tiene como objetivo controlar la visualizacion de 
+// los displays a traves de la UNIDAD y DECENA, apagando y 
+// prendiendo los mismos dependiendo el numero a mostrar
+void digitoEncendido(int numero) 
+{
+    if (numero == UNIDAD) 
+    {
+        digitalWrite(UNIDAD, LOW);
+        digitalWrite(DECENA, HIGH);
+ 		delay(TIMEDISPLAYON);
+    }
+    else if (numero == DECENA) 
+    {
+        digitalWrite(UNIDAD, HIGH);
+        digitalWrite(DECENA, LOW);
+		delay(TIMEDISPLAYON);
+    } 
+    else 
+    {
+        digitalWrite(UNIDAD, HIGH);
+        digitalWrite(DECENA, HIGH);
+		delay(TIMEDISPLAYON);
+    }
+}
+
+  
+void apagarDisplays() {
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+    digitalWrite(D, LOW);
+    digitalWrite(E, LOW);
+    digitalWrite(F, LOW);
+    digitalWrite(G, LOW);
+}
+
+         
+//FUNCION: ES PRIMO
+bool esPrimo(int num) 
+{
+  int contadorPrimo = 0;  // Inicializamos el contador de números primos a 0
+
+  for (int i = 1; i <= num; i++) 
+  {
+    if (num % i == 0) 
+    {
+      contadorPrimo += 1;
+    }
+  }
+
+  if (contadorPrimo == 2) 
+  {
+    return true;  // Si tiene exactamente 2 divisores, es primo
+  } else 
+  {
+    return false;  // De lo contrario, no es primo
+  }
+}
